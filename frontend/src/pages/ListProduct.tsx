16eaab95bcd6a24a98cbee4e373d2316000c16b8
Joao Oliveira <joao.oliveira@directy.com.br>
2025-01-30 15:33:43 -0300

    feat: Criado novo campo dentro de ListProduct.tsx

--- frontend/src/pages/ListProduct.tsx
+++ frontend/src/pages/ListProduct.tsx
@@ -8,28 +8,72 @@ interface Produto {
     preco: number; 
 }
 
+
 const ListProduct: React.FC = () => {   
     const [produtos, setProdutos] = useState<Produto[]>([]);
-
-    useEffect(() => {
-        axios
-        .get("http://localhost:3000/produtos") 
-        .then((response) => {
-          setProdutos(response.data);
-        })
-        .catch((error) => {
-          console.error("Erro ao buscar produtos:", error);
+    const [mostrarFormulario, setMostrarFormulario] = useState(false); //Criado para armazenar o novo produto 
+    const [novoProduto, setNovoProduto] = useState<Produto>({          //Criado para armazenar o novo produto 
+            id:0,
+            nome: "",
+            descricao: "",
+            preco: 0,
         });
+        
+    
+        useEffect(() => {
+            buscarProdutos();
+        }, []);
 
-      }, []);
+        const buscarProdutos = async () => { 
+            try {
+                const response = await axios.get("http://localhost:3000/produtos");
+                setProdutos(response.data);
+            } catch (error) {
+                console.error("Erro ao buscar produtos", error)
+            }
+        }
 
+        const handleSalvar = async () => {
+            try {
+                await axios.post("http://localhost:3000/produtos", novoProduto);
+                setMostrarFormulario(false);
+                setNovoProduto({id: 0, nome: "", descricao: "", preco: 0});
+                buscarProdutos(); 
+            } catch (error) {
+                console.log("erro ao adicionar produto", error);
+            }
+            };
 
-      console.log("produtos", produtos)
 
          return ( 
             <div>
             <h1>Lista de produtos</h1>
+            
+            <button onClick={() => setMostrarFormulario(true)}>Adicionar Produto</button> 
 
+            {mostrarFormulario && (
+        <div>
+          <input 
+            type="text" 
+            placeholder="Nome" 
+            value={novoProduto.nome} 
+            onChange={(e) => setNovoProduto({ ...novoProduto, nome: e.target.value })}
+          />
+          <input 
+            type="text" 
+            placeholder="Descrição" 
+            value={novoProduto.descricao} 
+            onChange={(e) => setNovoProduto({ ...novoProduto, descricao: e.target.value })}
+          />
+          <input 
+            type="number" 
+            placeholder="Preço" 
+            value={novoProduto.preco} 
+            onChange={(e) => setNovoProduto({ ...novoProduto, preco: parseFloat(e.target.value) })}
+          />
+          <button onClick={handleSalvar}>Salvar</button>
+        </div>
+      )}
             {produtos.length === 0 ? (
                 <p>Nenhum produto encontrado.</p>
             ) : (

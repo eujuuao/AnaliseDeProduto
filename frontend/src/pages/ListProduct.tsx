6c5e1a322f5805531b233cf77ac198b8a81cc3f7
Joao Oliveira <joao.oliveira@directy.com.br>
2025-01-30 11:20:22 -0300

    feat: array de produtos mapeado

--- frontend/src/pages/ListProduct.tsx
+++ frontend/src/pages/ListProduct.tsx
@@ -1,16 +1,21 @@
 import React, { useState, useEffect} from "react";
 import axios from "axios"; 
 
+interface Produto {
+    id: number;
+    nome: string;
+    descricao: string;
+    preco: number; 
+}
+
 const ListProduct: React.FC = () => {   
-
-    const [produtos, setProdutos] = useState();
-
+    const [produtos, setProdutos] = useState<Produto[]>([]);
 
     useEffect(() => {
         axios
-        .get("http://localhost:3000/produtos") // Chama a API para buscar os produtos
+        .get("http://localhost:3000/produtos") 
         .then((response) => {
-          setProdutos(response.data); // Atualiza o estado com os produtos recebidos
+          setProdutos(response.data);
         })
         .catch((error) => {
           console.error("Erro ao buscar produtos:", error);
@@ -21,11 +26,23 @@ const ListProduct: React.FC = () => {
 
       console.log("produtos", produtos)
 
-    return ( 
+         return ( 
             <div>
             <h1>Lista de produtos</h1>
+
+            {produtos.length === 0 ? (
+                <p>Nenhum produto encontrado.</p>
+            ) : (
+            <ul>
+                {produtos.map((produto) => (
+                    <li key={produto.id}>
+                        <strong>{produto.nome}</strong> - {produto.descricao} - R${produto.preco.toFixed(2)}
+                    </li>
+                ))}
+            </ul>
+            )} 
             </div>
         );
     };
 
-    export default ListProduct
+    export default ListProduct;
